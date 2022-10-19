20e2e1271c91fe1440cce8b819c8a0001ae0e8c4
ispan <ispan@DESKTOP-PAKSETB>
2022-10-19 13:29:47 +0800

    加入專案檔案。

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.Write("請輸入數字: ");
+			string input = Console.ReadLine();
+			int i = 1;
+
+			for (int k = Convert.ToInt32(input); k > 0; k--) 
+			{
+				i *= k;
+			}
+
+			Console.WriteLine($"{input}的乘積為{i}");
+
+		}
+	}
+}
